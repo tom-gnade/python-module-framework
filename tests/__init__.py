12e4d8120d76be0143721bba8843ad8ba3d3e4b7
Tom Gnade <tom.gnade@gmail.com>
2025-02-26 17:28:57 -0500

    renamed folder and made a few additions to test cases.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,31 @@ This package contains tests for all components of the framework:
 - log_manager.py: Logging system
 - config_manager.py: Configuration management
 """
+
+# Import test modules to make them discoverable
+from .test_module_base import TestLogLevel, TestConfigParam, TestDependencyClass
+from .test_module_base import TestBaseModule, TestBaseComponent, TestValidators, TestHelperFunctions
+
+from .test_log_manager import TestLogEvent, TestLogManager, TestComponentLogger, TestHelperFunctions as LogHelperFunctions
+
+from .test_config_manager import TestConfigManager, TestConfigValue, TestHelperFunctions as ConfigHelperFunctions
+
+# Define what's available via import *
+__all__ = [
+    # module_base tests
+    "TestLogLevel",
+    "TestConfigParam", 
+    "TestDependencyClass", 
+    "TestBaseModule", 
+    "TestBaseComponent", 
+    "TestValidators",
+    
+    # log_manager tests
+    "TestLogEvent",
+    "TestLogManager",
+    "TestComponentLogger",
+    
+    # config_manager tests
+    "TestConfigManager",
+    "TestConfigValue"
+]
